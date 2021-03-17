942b73e99d683c1baeedc31b80251ca1fd32e216
Julie Ann <julieanniwinski@gmail.com>
2021-03-17 18:10:11 -0400

    fixed spelling

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ const FILES_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.webmanifest',
-  '/style.css',
+  '/styles.css',
   '/index.js',
   '/db.js',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x192.png' 
+  '/icons/icon-512x512.png' 
 ];
 
 const STATIC_CACHE = "static-cache-v2";
